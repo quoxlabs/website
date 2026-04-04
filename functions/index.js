e345afbb9c07d0afe3274f0d17cc734b654975b9
KnorpelSenf <shtrog@gmail.com>
2026-04-04 14:47:58 +0200

    Fix redirect

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 export async function onRequestGet(context) {
   const accept = context.request.headers.get("Accept") ?? "";
   if (!accept.includes("text/html")) {
-    return Response.redirect("https://jsr.io/@quoxlabs/quox", 302);
+    return Response.redirect("jsr:@quoxlabs/quox", 302);
   }
   return context.env.ASSETS.fetch(context.request);
 }
