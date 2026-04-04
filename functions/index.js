eb4e7aea186bba1d73dcc4793ea29712a1e5cf10
KnorpelSenf <shtrog@gmail.com>
2026-04-04 15:06:08 +0200

    Set missing headers

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,12 @@
 export async function onRequestGet(context) {
   const accept = context.request.headers.get("Accept") ?? "";
   if (!accept.includes("text/html")) {
-    return new Response('export * from "jsr:@quoxlabs/quox"\n');
+    return new Response('export * from "jsr:@quoxlabs/quox"\n', {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/typescript; charset=utf-8",
+      }
+    });
   }
   return context.env.ASSETS.fetch(context.request);
 }
